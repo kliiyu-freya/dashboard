5ce9d5235eb0c3894279ff07299b53d22f979f93
Kliiyu <adri2007900@gmail.com>
2024-12-19 14:23:48 +0100

    trying nginx stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  base: '/',
   integrations: [tailwind()],
   server: {
     port: 6673
